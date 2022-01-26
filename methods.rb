7fcf53fd0d848d113ab8833ce88511f3a8054d53
Jesse Brown <Brownjesse8895@yahoo.com>
2022-01-26 17:57:52 -0600

    commit tree 69fee8171f371082a10d14a22d4f400d32855c34

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
-# Your code here!
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+    
+
+end
+
+
+def add(num1, num2)
+    num1 + num2
+end 
+
+
+
+def halve(num1)
+    puts num1
+    puts num1.class
+    if num1.class != Integer
+    return nil
+    end
+    return num1 / 2
+end
+
+
